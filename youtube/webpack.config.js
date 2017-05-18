5eb34ae788db7319840c39f69fcc1d4605cb8f30
anton-paskanny <anton.paskanny@gmail.com>
2017-05-18 15:38:56 +0300

    youtube/fix: fix eslint errors in webpack config + add packages

--- youtube/webpack.config.js
+++ youtube/webpack.config.js
@@ -1,60 +1,60 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlusPlugin = require('extract-text-webpack-plugin');
-var path = require('path');
-var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlusPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
-var isProd = process.env.NODE_ENV === 'production';
-var cssDev = ['style-loader', 'css-loader', 'sass-loader'];
-var cssProd = ExtractTextPlusPlugin.extract({
-     fallback: 'style-loader',
-     use: ['css-loader', 'sass-loader'],
-     publicPath: '/build'
-})
-var cssConfig = isProd ? cssProd : cssDev;
+const isProd = process.env.NODE_ENV === 'production';
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = ExtractTextPlusPlugin.extract({
+    fallback: 'style-loader',
+    use: ['css-loader', 'sass-loader'],
+    publicPath: '../'
+});
+const cssConfig = isProd ? cssProd : cssDev;
 
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/bundle.min.js',
-  },
-  module: {
-      rules: [
-        {
-            test: /\.scss$/,
-            use: cssConfig
-        },
-        {
-            test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-            use: 'file-loader?name=[name].[ext]&outputPath=fonts/fontello/'
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        },
-        {
-            test: /\.jpg$/,
-            use: 'file-loader?name=[name].[ext]&outputPath=img/'
-        }]
-  },
-  devServer: {
-      contentBase: path.join(__dirname, "build"),
-      compress: true,
-      hot: true,
-      open: true
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          template: './src/index.html'
-      }),
-      new ExtractTextPlusPlugin({
-          filename: 'css/style.css',
-          disable: !isProd,
-          allChunks: true
-      }),
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin()
-  ]
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: cssConfig
+            },
+            {
+                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: 'file-loader?name=[name].[ext]&outputPath=fonts/fontello/'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.jpg$/,
+                use: 'file-loader?name=[name].[ext]&outputPath=img/'
+            }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        hot: true,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlusPlugin({
+            filename: 'css/style.min.css',
+            disable: !isProd,
+            allChunks: true
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
 };
