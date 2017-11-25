29db18ff45c5b5c5c5383bc12af1c654f86fa35e
anton-paskanny <anton.paskanny@gmail.com>
2017-11-25 23:57:09 +0300

    Add service for requests

---
+++ news-app/services/requests.js
@@ -0,0 +1,38 @@
+class requestsServiceClass {
+  constructor() {
+    this.apiKey = '88323f990dab404cb1369f23f88a2053';
+    this.reqUrlPrefix = 'https://newsapi.org/v2';
+    this.reqAllNewsParam = 'everything';
+    this.reqTopHeadlinesParam = 'top-headlines';
+    this.reqSourcesParam = 'sources';
+    this.defaultParams = {
+      country: 'us',
+      language: 'en',
+      sources: [
+        'bbc-news',
+        'cnn',
+        'fortune',
+        'mtv-news',
+        'the-telegraph'
+      ]
+    }
+  }
+  getTopHeadlinesURL() {
+    const sources = this.defaultParams.sources.join(',');
+
+    return `${this.reqUrlPrefix}/${this.reqTopHeadlinesParam}?sources=${sources}&language=${this.defaultParams.language}&country=${this.defaultParams.country}&apiKey=${this.apiKey}`;
+  }
+  getNewsSourcesURL() {
+    return `${this.reqUrlPrefix}/${this.reqSourcesParam}?language=${this.defaultParams.language}&county=${this.defaultParams.country}&apiKey=${this.apiKey}`;
+  }
+  getTopHeadlinesNews() {
+    const url = this.getTopHeadlinesURL();
+
+    return fetch(url).then(data => data.json());
+  }
+  getNewsSources() {
+    const url = this.getNewsSourcesURL();
+
+    return fetch(url).then(data => data.json());
+  }
+}
