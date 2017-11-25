1d68d8bc98472e404feb879d35d931bd3c764ce0
anton-paskanny <anton.paskanny@gmail.com>
2017-11-25 23:57:29 +0300

    Add changes for components

--- news-app/components/newsList.js
+++ news-app/components/newsList.js
@@ -1,8 +1,30 @@
-class sourcesList {
-  constructor() {
-
+class newsListClass {
+  getNews(articles) {
+    return articles.map(article => (
+      `<article class="news-article">
+          <img class="news-article__img" src=${article.urlToImage} alt=${article.title}>
+          <div class="news-article__text-content">
+            <h2 class="news-article__title">${article.title}</h2>
+            <p class="news-article__desc">${article.description}</p>
+            <a class="class="news-article__link" href=${article.url} target="_blank">Read more</a>
+          </div>
+       </article>`
+    )).join('');
   }
-  render() {
+  generateTemplate(articles) {
+    const newsItems = this.getNews(articles),
+          newsComponent = document.createElement('div');
 
+    newsComponent.className = 'news-wrapper';
+
+    newsComponent.innerHTML = newsItems;
+
+    return newsComponent;
   }
 }
+
+/*
+<figure class="news-article__img-wrapper">
+  <img class="news-article__img" src=${article.urlToImage} alt=${article.title}>
+</figure>
+*/
--- news-app/components/sourcesList.js
+++ news-app/components/sourcesList.js
@@ -1,8 +1,22 @@
-class sourcesList {
-  constructor() {
-
+class sourcesListClass {
+  getListItems(sources) {
+    return sources.map(elem => (
+      `<label class="filter__label">
+        <input class="filter__input" type="checkbox" value=${elem.id}>
+        <span  class="filter__text">${elem.name}</span>
+      </label>`
+    )).join('');
   }
-  render() {
-    
+  generateTemplate(sources) {
+    const items = this.getListItems(sources),
+          form = document.createElement('form'),
+          title = document.createElement('h3');
+
+    form.className = 'filter';
+    title.className = 'filter__title'
+
+    form.innerHTML = items;
+
+    return form;
   }
 }
