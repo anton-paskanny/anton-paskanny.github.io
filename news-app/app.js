13fc368689335cc86ca0567d46ca8bf689cbc152
anton-paskanny <anton.paskanny@gmail.com>
2017-11-28 02:05:33 +0300

    Modify app.js, styles,index.html

--- news-app/app.js
+++ news-app/app.js
@@ -1,42 +1,54 @@
 (function() {
 
+  // entry point
   const App = document.querySelector('.root');
 
-  const requestsService = new requestsServiceClass();
-
   // classes
-  const sidebar = new SidebarClass();
-  const pageContent = new PageContentClass();
-  const sourcesList = new sourcesListClass();
-  const newsList= new newsListClass();
-  const toggleBtn = new ToggleBtnClass();
+  const sidebar = new Sidebar();
+  const spinner = new Spinner();
+  const pageContent = new PageContent();
+  const sourcesList = new SourcesList();
+  const newsList= new NewsList();
+  const toggleBtn = new ToggleBtn();
+  const footer = new Footer();
 
+  // services
+  const requestsService = new RequestsService();
+
+  // checkbox config
   const checkboxHandlerConfig = {
-    getEverythingNews: requestsService.getEverythingNews.bind(requestsService),
-    updateNewsList: newsList.updateComponent.bind(newsList)
-  }
+    getTopHeadlinesNews: requestsService.getTopHeadlinesNews.bind(requestsService),
+    updateNewsList: newsList.updateComponent.bind(newsList),
+    toggleSpinner: spinner.toggle.bind(spinner)
+  };
 
+  // get components
   const toggleBtnComponent = toggleBtn.getComponent();
+  const footerComponent = footer.getComponent();
+  const spinnerComponent = spinner.getComponent();
 
-  sourcesList.initCheckboxHandler(checkboxHandlerConfig);
+  // show spinner as we are going to fetch data
+  App.append(spinnerComponent);
+  spinner.toggle();
 
-  App.parentNode.append(toggleBtnComponent);
+  Promise.all([requestsService.getTopHeadlinesNews(), requestsService.getNewsSources()]).then(([{articles}, {sources}]) => {
+    const newsListComponent = newsList.getComponent(articles);
+    const pageContentComponent = pageContent.getComponent(newsListComponent);
+    const sourcesListComponent = sourcesList.getComponent(sources);
+    const sidebarComponent = sidebar.getComponent(sourcesListComponent);
 
-  // get news by default
-  requestsService.getTopHeadlinesNews().then(({articles}) => {
-    const newsListComponent = newsList.getComponent(articles),
-          pageContentComponent = pageContent.getComponent(newsListComponent);
-
-    App.append(pageContentComponent);
-  });
-
-  // get sources by default
-  requestsService.getNewsSources().then(({sources}) => {
-    const sourcesListComponent = sourcesList.getComponent(sources),
-          sidebarComponent = sidebar.getComponent(sourcesListComponent);
-
+    sourcesList.initCheckboxHandler(checkboxHandlerConfig);
     toggleBtn.initToggleBtnHandler(sidebarComponent);
 
+    pageContentComponent.append(footerComponent)
+
+    App.parentNode.append(toggleBtnComponent);
+    App.append(pageContentComponent);
     App.prepend(sidebarComponent);
+
+    // hide spinner as we have already downloaded all data
+    setTimeout(() => {
+      spinner.toggle();
+    }, 1000)
   });
 })();
