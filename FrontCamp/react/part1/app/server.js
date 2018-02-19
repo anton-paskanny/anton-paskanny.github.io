6eac1e1f099541aa075ef7942e7a271cb34d454d
Anton Paskanny <anton.paskanny@gmail.com>
2018-02-19 14:52:57 +0300

    Update server.js

--- FrontCamp/react/part1/app/server.js
+++ FrontCamp/react/part1/app/server.js
@@ -7,9 +7,12 @@ import App from './components/App.jsx';
 const port = 3000;
 
 const app = express();
+app.use(express.static('dist'));
 
 app.get('/', function(res, req) {
-  res.send(template(renderToString(<App />)))
+  const component = renderToString(<App />);
+  const indexPage = template(component);
+  res.send(indexPage);
 });
 
 app.listen(3000);
