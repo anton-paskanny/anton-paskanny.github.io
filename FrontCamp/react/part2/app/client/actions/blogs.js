00f9e7c44bdaf62f558a9a5dc7143aead2027b88
Anton Paskanny <anton.paskanny@gmail.com>
2018-03-14 23:13:36 +0300

    Update actions

--- FrontCamp/react/part2/app/client/actions/blogs.js
+++ FrontCamp/react/part2/app/client/actions/blogs.js
@@ -1,65 +1,21 @@
-import serverConfig from '../../server/config';
-
 export const ADD_BLOG = "ADD_BLOG";
 export const DELETE_BLOG = "DELETE_BLOG";
 export const FETCH_BLOGS = "FETCH_BLOGS";
 
-const baseUrl = `http://${serverConfig.db.host}:${serverConfig.port}/${serverConfig.routes.blogs.base}`;
 
-export const addBlog = blog => {
-  return dispatch => {
+export const addBlog = blog => ({
+  type: ADD_BLOG,
+  blog
+});
 
-    return fetch(baseUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(blog),
-      credentials: 'include',
-      mode: 'cors'
-    })
-    .then(res => res.json())
-    .then(res => {
-      dispatch({
-        type: ADD_BLOG,
-        blog: res.blog
-      });
-    });
+export const deleteBlog = _id => ({
+  type: DELETE_BLOG,
+  _id
+});
 
-  }
-}
+export const fetchBlogs = blogs => ({
+  type: FETCH_BLOGS,
+  blogs
+});
 
-export const deleteBlog = id => {
-  return dispatch => {
-
-    return fetch(`${baseUrl}/${encodeURI(id)}`, {
-      method: 'DELETE',
-      mode: 'cors',
-      credentials: 'include'
-    })
-    .then(res => res.json())
-    .then(res => {
-      dispatch({
-        type: DELETE_BLOG,
-        _id: res._id
-      });
-    });
-  }
-}
-
-export const fetchBlogs = () => {
-  return dispatch => {
-
-    return fetch(baseUrl, {
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include'
-    })
-    .then(res => res.json())
-    .then(res => {
-      dispatch({
-        type: FETCH_BLOGS,
-        blogs: res.blogs
-      });
-    });
-
-  }
-}
+export default { addBlog, deleteBlog, fetchBlogs };
