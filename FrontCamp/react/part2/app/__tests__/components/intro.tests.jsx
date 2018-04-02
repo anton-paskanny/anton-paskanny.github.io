496615aa162a66d0606ff8d8aa8d855e495d75f5
Anton Paskanny <anton.paskanny@gmail.com>
2018-04-02 22:59:29 +0300

    Add snapshots tests

--- FrontCamp/react/part2/app/__tests__/components/intro.tests.jsx
+++ FrontCamp/react/part2/app/__tests__/components/intro.tests.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow, configure, mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 
 import Intro from '../../client/components/intro/Intro';
@@ -11,5 +12,10 @@ describe('Intro component', () => {
   test('should render Intro component', () => {
     const component = shallow(<Intro />);
     expect(component).toHaveLength(1);
+  });
+
+  test('should render correctly', () => {
+    const component = renderer.create(<Intro />).toJSON();
+    expect(component).toMatchSnapshot();
   });
 });
