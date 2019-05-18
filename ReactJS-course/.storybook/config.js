2726f7358cab2300de0aea5f974d246a806e90d9
Anton Paskanny <anton.paskanny@gmail.com>
2019-05-18 14:58:00 +0300

    Add styled-components to storybook

--- ReactJS-course/.storybook/config.js
+++ ReactJS-course/.storybook/config.js
@@ -1,6 +1,7 @@
-import { configure } from '@storybook/react';
+import * as React from 'react';
+import { addDecorator, configure } from '@storybook/react';
 
-import "../src/client/styles.css";
+import { CSSNormalize, GlobalDefaults } from '../src/client/commonStyles';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
@@ -8,4 +9,13 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
+const withGlobal = (cb) => (
+  <React.Fragment>
+    <CSSNormalize />
+    <GlobalDefaults />
+    {cb()}
+  </React.Fragment>
+);
+
+addDecorator(withGlobal);
 configure(loadStories, module);
